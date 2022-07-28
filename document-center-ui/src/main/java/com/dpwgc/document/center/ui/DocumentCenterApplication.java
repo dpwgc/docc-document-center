db230f25e73d4dc8d5d9a778919d65b137011324
dpwgc <281148314@qq.com>
2022-07-28 11:44:58 +0800

    bean问题修复

--- document-center-ui/src/main/java/com/dpwgc/document/center/ui/DocumentCenterApplication.java
+++ document-center-ui/src/main/java/com/dpwgc/document/center/ui/DocumentCenterApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.dpwgc.document.center.infrastructure.util")
 @ComponentScan("com.dpwgc.document.center.infrastructure.component")
 @ComponentScan("com.dpwgc.document.center.infrastructure.repository")
-@MapperScan("com.dpwgc.message.center.infrastructure.dal")
+@MapperScan("com.dpwgc.document.center.infrastructure.dal")
 @SpringBootApplication
 public class DocumentCenterApplication {
 
