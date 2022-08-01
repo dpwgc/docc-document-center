a25d10cda9d5003410744c1500b5634e83c25d59
dpwgc <281148314@qq.com>
2022-08-01 14:04:35 +0800

    SQL文件

---
+++ sql/docc_document_center_product.sql
@@ -0,0 +1,27 @@
+create table category
+(
+    id            bigint unsigned auto_increment
+        primary key,
+    app_id        varchar(64)  not null,
+    category_id   varchar(64)  not null,
+    parent_id     varchar(64)  not null,
+    category_name varchar(100) not null,
+    detail        text         not null,
+    score         bigint       not null,
+    status        int          not null,
+    create_time   bigint       not null,
+    update_time   bigint       not null
+)
+    auto_increment = 4;
+
+create table tag
+(
+    id          bigint unsigned auto_increment
+        primary key,
+    app_id      varchar(64)  not null,
+    tagName     varchar(100) not null,
+    number      bigint       not null,
+    status      int          not null,
+    create_time bigint       not null,
+    update_time bigint       not null
+);
