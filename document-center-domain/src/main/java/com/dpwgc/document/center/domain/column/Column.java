f2f02279f05209716405b9b7495bf1578d238dca
dpwgc <281148314@qq.com>
2022-11-24 17:13:22 +0800

    专栏更新

--- document-center-domain/src/main/java/com/dpwgc/document/center/domain/column/Column.java
+++ document-center-domain/src/main/java/com/dpwgc/document/center/domain/column/Column.java
@@ -1,4 +1,94 @@
 package com.dpwgc.document.center.domain.column;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Column {
+
+    /**
+     * 主键id
+     */
+    private String id;
+
+    /**
+     * 所属应用id
+     */
+    private String appId;
+
+    /**
+     * 专栏id
+     */
+    private String columnId;
+
+    /**
+     * 作者/发布者id
+     */
+    private String authorId;
+
+    /**
+     * 专栏名称
+     */
+    private String columnName;
+
+    /**
+     * 专栏详情
+     */
+    private String detail;
+
+    /**
+     * 专栏附加内容（自定义）
+     */
+    private String extra;
+
+    /**
+     * 专栏推荐分值（自由分配，返回分类列表时将按此字段对分类进行排序）
+     */
+    private Long score;
+
+    /**
+     * 专栏属性（自定义，例：1-热门专栏、0-普通专栏）
+     */
+    private Integer attr;
+
+    /**
+     * 专栏类型（自定义，例：1-官方专栏，0-普通专栏）
+     */
+    private Integer type;
+
+    /**
+     * 专栏状态（0-删除，1-正常）
+     */
+    private Integer status;
+
+    /**
+     * 专栏创建时间
+     */
+    private Long createTime;
+
+    /**
+     * 专栏更新时间
+     */
+    private Long updateTime;
+
+    protected Column create(String appId,String columnId,String authorId,String columnName,String detail,String extra,Long score,Integer attr,Integer type) {
+
+        this.status = 1;
+
+        this.appId = appId;
+        this.columnId = columnId;
+        this.authorId = authorId;
+        this.columnName = columnName;
+        this.detail = detail;
+        this.extra = extra;
+        this.score = score;
+        this.attr = attr;
+        this.type = type;
+
+        this.createTime = System.currentTimeMillis();
+        this.updateTime = System.currentTimeMillis();
+
+        return this;
+    }
 }
--- document-center-domain/src/main/java/com/dpwgc/document/center/domain/column/ColumnFactory.java
+++ document-center-domain/src/main/java/com/dpwgc/document/center/domain/column/ColumnFactory.java
@@ -1,4 +1,7 @@
 package com.dpwgc.document.center.domain.column;
 
 public class ColumnFactory {
+    public Column create(String appId,String columnId,String authorId,String columnName,String detail,String extra,Long score,Integer attr,Integer type) {
+        return new Column().create(appId, columnId, authorId, columnName, detail, extra, score, attr, type);
+    }
 }
